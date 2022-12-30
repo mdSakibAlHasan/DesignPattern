a9cd3d73f18478ce46eb9eccc39ddd610ef9be3b
mdSakibAlHasan <bsse1209@iit.du.ac.bd>
2022-12-30 19:39:36 +0600

    Solution complete

---
+++ Class_project/src/codeSmellAssignment/PrintJob.java
@@ -0,0 +1,21 @@
+package codeSmellAssignment;
+
+import javax.swing.text.Document;
+import java.util.Queue;
+
+public class PrintJob {
+    HighTonerSaveMode highTonerSaveMode = new HighTonerSaveMode();
+    LowTonerSaveMode lowTonerSaveMode = new LowTonerSaveMode();
+    MediumTonerSaveMode mediumTenorSaveMode = new MediumTonerSaveMode();
+    Queue<Object> PrintRequest ;
+
+    public void pullJob()
+    {
+
+    }
+
+    public void changePriority()
+    {
+        
+    }
+}
